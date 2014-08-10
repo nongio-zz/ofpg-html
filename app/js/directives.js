d90da3c74c64b6ba3b3946e5b25845e32c6af448
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2014-08-10 07:23:17 +0200

    browse folder directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,7 +1,16 @@
 angular.module('ofPG.directives', [
     'ofPG.services'
 ])
-
+.directive('noDrag', [function() {
+    return {
+            template: '<a onmousedown="return false"><span ng-transclude></span></a>',
+            restrict: 'A',
+            replace: true,
+            transclude: true,
+            link: function(scope, iElement, iAttrs) {
+            }
+        };
+    }])
 .directive('navigationBar', [function() {
     return {
             templateUrl: 'templates/_navigationbar.html',
@@ -13,11 +22,49 @@ angular.module('ofPG.directives', [
     }])
 .directive('section', [function() {
     return {
-            template: '<div ng-mouseover="onHover()" ng-transclude></div>',
+            template: '<div ng-transclude></div>',
             restrict: 'E',
             transclude: true,
             link: function(scope, iElement, iAttrs) {
-                console.log('navigation bar');
+                var hintbaloon = iElement.find('.hint');
+                iElement.hover(function(){
+                    iElement.addClass('focus');
+                    hintbaloon.show();
+                    setTimeout(function(){
+                        hintbaloon.addClass('visible');
+                    });
+                },
+                function(){
+                    iElement.removeClass('focus');
+                    hintbaloon.removeClass('visible');
+                    setTimeout(function(){
+                        hintbaloon.hide();
+                    });
+                });
+                hintbaloon.hide();
+
+            }
+        };
+    }])
+.directive('browseFolder', [function() {
+    return {
+            templateUrl: 'templates/_browsefolder.html',
+            restrict: 'E',
+            scope: {},
+            link: function(scope, iElement, iAttrs) {
+                // https://github.com/rogerwang/node-webkit/wiki/File-dialogs#pure-javascript
+                var chooser = iElement.find('.folderDialog');
+                chooser.change(function(evt) {
+                    var folder = $(this).val();
+                    if(folder != '') {
+                        scope.$apply(function(){
+                            scope.folder = folder;
+                        });
+                    }
+                });
+                iElement.find('button').click(function(){
+                    chooser[0].click();
+                });
             }
         };
     }])
