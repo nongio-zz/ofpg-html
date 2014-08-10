5b68ecf1aaec6c6c2d06c1ffa7d4a77c04b305ac
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2014-08-10 10:17:20 +0200

    osx menu needed to enable copy - paste - close shortcut

--- app/js/app.js
+++ app/js/app.js
@@ -14,3 +14,13 @@ angular.module('ofPG', ['ngRoute' , 'ngAnimate', 'ofPG.services', 'ofPG.controll
     })
     .otherwise({redirectTo : '/main'});
 });
+
+
+// enabled default window menu
+var gui = require('nw.gui');
+var win = gui.Window.get();
+var nativeMenuBar = new gui.Menu({ type: "menubar" });
+if(nativeMenuBar.createMacBuiltin) {
+    nativeMenuBar.createMacBuiltin("OF");
+    win.menu = nativeMenuBar;
+}
